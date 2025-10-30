7fcc69d9db19af51ac0ef09b536abc85cbebbfc0
yvygyyth <1589937631@qq.com>
2025-10-30 10:13:35 +0800

    feat: 添加@别名

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
